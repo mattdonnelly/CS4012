4a46b59ddbccbe0713298be0c2d51ae97e19b692
Matt Donnelly <mattdonnelly@me.com>
2014-11-20 16:10:42 +0000

    Only run parallel MapReduce for testing

--- Assignment 1/src/Main.hs
+++ Assignment 1/src/Main.hs
@@ -9,6 +9,5 @@ main :: IO ()
 main = do
     args <- getArgs
     contents <- readFile $ head args
-    let frequencies1 = Part01.wordFrequency $ lines contents
     let frequencies2 = Part02.wordFrequency $ lines contents
     print frequencies2
